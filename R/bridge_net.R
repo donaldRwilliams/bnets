783492b146e1677ea7dd7b2ae136589a0c623b39
donaldRwilliams <brycewayne03.29@gmail.com>
2017-10-18 21:05:18 -0700

    commit

--- R/bridge_net.R
+++ R/bridge_net.R
@@ -1,4 +1,6 @@
 #' @export
+#' @import rstan
+#' @import parallel
 #' @useDynLib bnets, .registration = TRUE
 bridge_net <- function(X, prior_scale = 1, chains = 4, models = 1,
                        scale_seq = scale_seq, iter = 2000,

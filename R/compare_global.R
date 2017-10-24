d8b8d211191098c6bb1ff6ec3a577e0bed8738cc
donaldRwilliams <drwwilliams@ucdavis.edu>
2017-10-24 06:24:34 -0700

    compare_global

--- R/compare_global.R
+++ R/compare_global.R
@@ -10,17 +10,30 @@
 
 compare_global <- function(x){
 
-  c_names <- names(loo$loo_list)
-  assess_values <- t(combn(c_names , 2))
+  # get list names that correspond to priors
+  list_names <- names(loo$loo_list)
+
+  # combn to evaluate pairwise loo
+  assess_values <- t(combn(list_names , 2))
+
+  # function for mapply
   fun_mapply <- function(prior_scale_1, prior_scale_2){
       loo::compare(loo$loo_list[[prior_scale_1]],
                    loo$loo_list[[prior_scale_2]])
     }
-    m <- t(mapply(fun_mapply,  assess_values[,2], assess_values[,1]))
-    dat <- suppressWarnings(data.frame(assess_values, round(m, 3)))
-    looic <- dat$elpd_diff * -2
-    se <- dat$se * 2
-    results <- data.frame(dat, looic, se)
+
+  # temporary results
+  temp_results1 <- t(mapply(fun_mapply,
+                           assess_values[,2], assess_values[,1]))
+
+  temp_results2 <- suppressWarnings(data.frame(assess_values, round(temp_results1, 3)))
+
+    looic <- temp_results2$elpd_diff * -2
+
+    se <- temp_results2$se * 2
+
+    results <- data.frame(temp_results2, looic, se)
+
     colnames(results) <- c("prior_1", "prior_2", "elpd_diff",
                            "elpd_se", "looic_diff", "looic_se")
     list(results = results)
